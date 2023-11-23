40aca0332b53e1c0a7bcaa9d23945d218063133f
eidoxy <adriansondang@gmail.com>
2023-11-23 20:37:23 +0700

    Update home page

--- views/Menu/script.js
+++ views/Menu/script.js
@@ -1,22 +1,59 @@
-// Create a varbable for select a buttons and menu
+// Swiper.js
+let swiperCards = new Swiper('.swiper', {
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        450: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        1000:{
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+        },
+        1200: {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+        },
+    },
+});
+
+// Membuat variabel untuk menyeleksi class filter-btn dan filterable-menu
 const filterButtons = document.querySelectorAll(".filter-btn button");
 const filterableMenu = document.querySelectorAll(".filterable-menu");
 
-// Define a function for filter menu
+// Membuat fungsi untuk filter menu
 const filterMenu = e => {
-    document.querySelector(".active").classList.remove("active", "active");
+    document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
 
-    // Iterate over each filterable menu
+    // Perulangan untuk menyeleksi class filterable-menu
     filterableMenu.forEach(menu => {
-        // Add "hide" class for each menu to hide them
+        // Menambahkan class "hide" dari setiap menu untuk menghilangkan konten
         menu.classList.add("hide");
-        // Check if the menu are selected with filter "all"
+        // Membuat fungsi filter "all"
         if (menu.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
             menu.classList.remove("hide");
         }
     });
 };
 
-// Add click listener to each filter button
+// Membuat click listener untuk setiap button yang di klik
 filterButtons.forEach(button => button.addEventListener("click", filterMenu));
